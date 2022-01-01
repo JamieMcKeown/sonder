661086ac05feef2304d55a4230ca3a6d1d732a03
Jamie <1995237@cstj.qc.ca>
2022-01-01 16:52:41 -0500

    initial commit

---
+++ database/migrations/2022_01_01_171940_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->int('quantity');
+            $table->string('city', 50);
+            $table->string('province_state', 50);
+            $table->string('country', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
