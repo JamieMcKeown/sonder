ec836081d8c4d571cfa4d9f9af714f57681dc189
Jamie <1995237@cstj.qc.ca>
2022-01-02 11:21:07 -0500

    landing page done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>rednos</title>
+        <title>Sonder Beverage</title>
 
           <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
@@ -12,13 +12,33 @@
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
+
+
     <body>
-        <!-- As a heading -->
-        <nav class="navbar navbar-light bg-light">
-        <div class="container-fluid">
-            <span class="navbar-brand mb-0 h1">Navbar</span>
-        </div>
+        <!-- header -->
+        <nav class="navbar  bg-danger">
+            <div class="container-fluid justify-content-center">
+                <span class="navbar-brand mb-0 fs-2" style="color:white">Sonder</span>
+            </div>
         </nav>
+
+        <!-- main -->
+        <div class="container-fluid" style="padding-top:20%">
+            <div class="card m-auto" style="width: 55%">
+                <img src="/img/fbBack.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Sonder Tonic</h5>
+                    <p class="card-text">The most delicious mix of Sangria and Tonic Water</p>
+                    <a href="#" class="btn btn-primary">Buy</a>
+                </div>
+            </div>
+        </div>
+      
+        <div class="container-fluid fixed-bottom mb-2 bg-danger color-white">
+            <p class="fs-4 text-center" style="color:white">Jamie McKeown</p>
+        </div>
+       
+        
 
     </body>
 </html>
